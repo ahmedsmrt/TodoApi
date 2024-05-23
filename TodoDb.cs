965b0d8b574ca7912109cd6a160129e3222d52b5
agasimdev <agasim@archeredu.com>
2024-05-22 22:31:55 -0400

    first commit

---
+++ TodoDb.cs
@@ -0,0 +1,11 @@
+using Microsoft.Entity.FrameworkCore;
+
+// A DbContext instance represents a session with the database and can be used to query and save instances of your entities. DbContext is a combination of the Unit Of Work and Repository patterns.
+class TodoDb : DbContext
+{
+    // Initializes a new instance of the DbContext class using the specified options. The OnConfiguring(DbContextOptionsBuilder) method will still be called to allow further configuration of the options.
+    public TodoDb(DBContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
